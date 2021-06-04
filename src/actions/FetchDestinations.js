1a491ad48c796dca75ef83143727454b25106273
Wurdsmith <wurdsmithing@gmail.com>
2021-06-04 00:19:04 -0500

    Changes the route of the fetchDestinations function to pull data from the main itinerary containing all destination objects.

--- src/actions/FetchDestinations.js
+++ src/actions/FetchDestinations.js
@@ -1,11 +1,11 @@
-export default function fetchLists() {
+export default function fetchDestinations() {
     return (dispatch) => {
-      fetch("http://localhost:3000/destinations")
+      fetch("http://localhost:3000/itineraries/1")//Fetches only the destinations from the main itinerary that contains all destinations.
       .then(function(response) {
         return response.json()
       })
       .then((destinationsArray) => {
-        dispatch({type: "FETCH_DESTINATIONS", payload: destinationsArray})
+        dispatch({type: "FETCH_DESTINATIONS", payload: destinationsArray.destinations})//Destinations is the key that points to an array of destination objects.
       })
     }
   }
